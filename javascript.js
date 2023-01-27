a1d4eab61fe4425d465512f3872f09434146e1ae
jayendraJoshi <jayjoshi@outlook.de>
2023-01-27 17:16:23 +0100

    create html,css and js file & add basic grid functionality
    add div container to html file to contain divs
    add divs through js dom methods for more flexibility
    apply css grid layout
    change background of divs when hovered over to make them visible to user

---
+++ javascript.js
@@ -0,0 +1,10 @@
+const divContainer =document.querySelector(".container")
+
+for(i=0;i<16*16;i++)
+{
+let Div=document.createElement("div");
+Div.setAttribute("class","colorDiv")
+divContainer.appendChild(Div);
+}
+
+
