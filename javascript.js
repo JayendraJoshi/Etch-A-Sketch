1d1dbf5f2b95991a1f493c396f1aeefdd788a893
jayendraJoshi <jayjoshi@outlook.de>
2023-01-27 19:10:13 +0100

    Enable user to change grid size dynamically
    add button to ask for a number when clicked
    create divs according to given number
    set grid-template-column & rows according to given number

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 const divContainer =document.querySelector(".container")
 
-for(i=0;i<16*16;i++)
-{
-let Div=document.createElement("div");
-Div.setAttribute("class","colorDiv")
-divContainer.appendChild(Div);
-}
+    document.querySelector("button").addEventListener("click",function(){
+    let n=prompt("Enter a number to apply grid size (example: 64 = 64x64)")
 
+    for(i=0;i<n*n;i++)
+    {
+        let Div=document.createElement("div");
+        Div.setAttribute("class","colorDiv");
+        divContainer.appendChild(Div);
+    }
+    divContainer.style.gridTemplateColumns=`repeat(${n},1fr)`;
+    divContainer.style.gridTemplateRows=`repeat(${n},1fr)`;
+
+})
 
