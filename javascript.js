0a1426fd10297406c34982285785ea17cae172bc
jayendraJoshi <jayjoshi@outlook.de>
2023-01-27 21:54:14 +0100

    Add randomRGBValue & reset method, replace hover with mouseover and mouseout events
    add randomRGB. method to change div colors randomly
    add reset method to clear container after user inputs new number
    replace hover with mouseover and mouseout events to apply randomRGB. method to selected divs

--- javascript.js
+++ javascript.js
@@ -1,16 +1,42 @@
 const divContainer =document.querySelector(".container")
 
-    document.querySelector("button").addEventListener("click",function(){
-    let n=prompt("Enter a number to apply grid size (example: 64 = 64x64)")
+    document.querySelector("button").addEventListener("click",function(){   
+        let n=prompt("Enter a number to apply grid size (example: 64 = 64x64)")
+        reset();
 
-    for(i=0;i<n*n;i++)
-    {
-        let Div=document.createElement("div");
-        Div.setAttribute("class","colorDiv");
-        divContainer.appendChild(Div);
-    }
-    divContainer.style.gridTemplateColumns=`repeat(${n},1fr)`;
-    divContainer.style.gridTemplateRows=`repeat(${n},1fr)`;
+        for(i=0;i<n*n;i++)
+        {
+            let Div=document.createElement("div");
+            Div.setAttribute("class","colorDiv");
+            divContainer.appendChild(Div);
+        }
+        divContainer.style.gridTemplateColumns=`repeat(${n},1fr)`;
+        divContainer.style.gridTemplateRows=`repeat(${n},1fr)`;
+        
+            let divs=document.querySelectorAll(".colorDiv");
 
-})
+                let r=randomRgbValue();
+                let g=randomRgbValue();
+                let b=randomRgbValue();
 
+                    divs.forEach(element => {
+                        element.addEventListener("mouseover",function(){               
+                        element.style.backgroundColor=`rgb(${r},${g},${b})`;
+        
+                        })
+                    });
+                    divs.forEach(element=>{
+                        element.addEventListener("mouseout",function(){
+                        element.style.backgroundColor="";
+                        })
+                    })             
+    });
+
+function randomRgbValue(max){
+    max=255;    
+  return Math.floor((Math.random()* max));
+}
+
+function reset(){
+  divContainer.textContent="";
+}
