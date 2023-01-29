a8d3ccec620e5b087872e7d2604667ff37600adb
jayendraJoshi <jayjoshi@outlook.de>
2023-01-29 18:14:03 +0100

    Improve painting controls
    paint/erase only when user clicks or holds a mouse button down to prevent accidental painting/erasing
    enable user to color/decolor single divs by clicking once
    enable user to color/decolor multiple divs by holding a mouse button down and hovering over them

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const divContainer =document.querySelector(".container")
 const slider=document.querySelector(".slider");
 const output=document.querySelector(".output")
+let mouseIsDown=false;
 
 slider.addEventListener("input",function(element){
 
@@ -22,34 +23,61 @@ slider.addEventListener("input",function(element){
                 let g=randomRgbValue();
                 let b=randomRgbValue();
 
-                    divs.forEach(element => {
-                        element.addEventListener("mouseover",function(){ 
-                        if(document.querySelector(".color").checked===true){
-                            element.style.backgroundColor=`rgb(${r},${g},${b})`;
-                            }
+                            //colors single div
+                    divs.forEach(element=>{
+                        element.addEventListener("click",function(){
+                             if(document.querySelector(".color").checked===true){
+                                    element.style.backgroundColor=`rgb(${r},${g},${b})`;                                 
+                                }           
+                                else if(document.querySelector(".erase").checked===true)
+                                        {   
+                                            element.style.backgroundColor="";                                    
+                                        }   
                         })
                     });
+                        //colors multiple divs
+                    divs.forEach(element => {                        
+                        element.addEventListener("pointerover",function(){                            
+                                if(document.querySelector(".color").checked===true && mouseIsDown===true){
+                                    element.style.backgroundColor=`rgb(${r},${g},${b})`;                                 
+                                    }           
+                                else if(document.querySelector(".erase").checked===true &&mouseIsDown===true)
+                                        {
+                                            element.style.backgroundColor="";                                    
+                                        }                                     
+                        })  
+                    });     
+                    //colors first of multiple divs
                     divs.forEach(element=>{
-                        element.addEventListener("mouseover",function(){
-                        if(document.querySelector(".erase").checked===true){
-                            element.style.backgroundColor="";
-                            }
+                        element.addEventListener("mousedown",function(){
+                            if(document.querySelector(".color").checked===true){
+                                    element.style.backgroundColor=`rgb(${r},${g},${b})`;                                 
+                                }           
+                            else if(document.querySelector(".erase").checked===true)
+                                    {
+                                        element.style.backgroundColor="";                                    
+                                    }  
                         })
-                    })             
+                    })
+
     });
 
 function randomRgbValue(max){
     max=255;    
   return Math.floor((Math.random()* max));
 }
-
 function reset(){
   divContainer.textContent="";
 }
-
 slider.addEventListener("input",function(element){
-
     output.textContent=this.value;
     console.log(element);
-
 })
+
+divContainer.addEventListener("pointerdown",function(){
+    mouseIsDown=true;
+})
+divContainer.addEventListener("pointerup",function(){
+    mouseIsDown=false;
+})
+
